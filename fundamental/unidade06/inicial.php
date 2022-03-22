5c296ac4ceb968e708d210a5188f7085115f6089
Andre Scaranto <ascaranto@gmail.com>
2022-03-21 21:39:46 -0300

    Add fundamental/padrao and fundamental/unidade06
    Adicionado o teste inicial do PHP, usando a função echo para imprimir no documento html.

---
+++ fundamental/unidade06/inicial.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Curso PHP FUNDAMENTAL</title>
+    </head>
+
+    <body>
+        <p><?php echo "André Scaranto"; ?></p>
+        <p><?php echo 5*4; ?></p>
+    </body>
+</html>
